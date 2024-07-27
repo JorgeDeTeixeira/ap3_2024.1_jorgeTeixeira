0cebdf3b39a751604855ef94723fc9c6e3e492d1
Jorge Teixeira <jorgedeteixeirademelo@gmail.com>
2024-07-26 21:39:45 -0300

    aula 3
    Exercicios da aula 3

--- unidade1/src/gabarito_aula3/TestaAlunoOO.java
+++ unidade1/src/gabarito_aula3/TestaAlunoOO.java
@@ -1,5 +1,19 @@
 package unidade1.src.gabarito_aula3;
 
 public class TestaAlunoOO {
+    public static void main(String[] args) {
+        Aluno jorge = new Aluno();
+
+        jorge.setNomeDoAluno("Jorge");
+        jorge.setNota1(7);
+        jorge.setNota2(8);
+        jorge.setNota3(9);
+
+        if (jorge.aprovadoPorMedia()) {
+            System.out.println("Aprovado por média!");
+        } else {
+            System.out.println("Não passou ein.");
+        }
+    }
 
 }
